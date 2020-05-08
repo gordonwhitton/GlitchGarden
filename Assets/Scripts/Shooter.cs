956e4c8fecf9afad55609fe9aaf30efe1cfc12c2
Rob Whitton <limehc@gmail.com>
2020-05-08 23:06:29 +0100

    first commit

---
+++ Assets/Scripts/Shooter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+
+    [SerializeField] GameObject projectile;
+
+    [SerializeField] GameObject gun;
+
+    public void Start()
+    {
+        
+    }
+
+    public void Fire()
+    {
+        Instantiate(projectile, gun.transform.position, transform.rotation);
+    }
+
+}
