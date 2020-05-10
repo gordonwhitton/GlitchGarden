34ac05f10288e0f80d90890c657d7f10644c619d
Rob Whitton <limehc@gmail.com>
2020-05-10 17:41:54 +0100

    next section

--- Assets/Scripts/DefenderSpawner.cs
+++ Assets/Scripts/DefenderSpawner.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class DefenderSpawner : MonoBehaviour
 {
 
-    [SerializeField] GameObject defender;
+    Defender defender;
+
+    private float offset = 0.5f;
 
     private void OnMouseDown()
     {
@@ -13,15 +15,45 @@ public class DefenderSpawner : MonoBehaviour
         SpawnDefender(GetSquareClicked());
     }
 
+    public void SetSelectedDefender(Defender defenderToSelect)
+    {
+        defender = defenderToSelect;
+    }
+
     private Vector2 GetSquareClicked()
     {
         Vector2 clickPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
         Vector2 worldPos = Camera.main.ScreenToWorldPoint(clickPos);
-        return worldPos;
+        return SnapToGrid(worldPos);
+    }
+
+    private Vector2 SnapToGrid(Vector2 rawWorldPosition)
+    {
+        //float newX = Mathf.RoundToInt(rawWorldPosition.x)+0.5f;// + offset;
+        //float newY = Mathf.RoundToInt(rawWorldPosition.y)+1f;// + offset - 0.5f;
+
+
+        float yOffset;
+
+        if(defender.name == "Trophy")
+        {
+            yOffset = 0.5f;
+        } else
+        {
+            yOffset = 0.5f;
+        }
+        float newX = Mathf.FloorToInt(rawWorldPosition.x) + 0.5f;// + offset;
+
+        float newY = Mathf.FloorToInt(rawWorldPosition.y) + yOffset;// + offset - 0.5f;
+
+        Debug.Log("Raw " + rawWorldPosition.x + " " + rawWorldPosition.y);
+        Debug.Log("New " + newX + " " + newY);
+
+        return new Vector2(newX, newY);
     }
 
     private void SpawnDefender(Vector2 worldPosition)
     {
-        GameObject newDefender = Instantiate(defender, worldPosition, Quaternion.identity) as GameObject;
+        Defender newDefender = Instantiate(defender, worldPosition, Quaternion.identity) as Defender;
     }
 }
