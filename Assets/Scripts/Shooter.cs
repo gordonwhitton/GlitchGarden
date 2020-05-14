7d677ba9f8626c0ada0427340ec6551b952d3e44
Rob Whitton <limehc@gmail.com>
2020-05-14 22:18:39 +0100

    box colliders on defenders and add axe

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -9,9 +10,64 @@ public class Shooter : MonoBehaviour
 
     [SerializeField] GameObject gun;
 
+    AttackerSpawner myLaneSpawner;
+
+    Animator animator;
+
     public void Start()
     {
-        
+        SetLaneSpawner();
+        animator = GetComponent<Animator>();
+    }
+
+
+    private void Update()
+    {
+        if(IsAttackerInLane())
+        {
+            Debug.Log("shoot attacker");
+            animator.SetBool("isAttacking", true);
+        }
+        else
+        {
+            Debug.Log("sit and wait");
+            animator.SetBool("isAttacking", false);
+        }
+    }
+
+    private void SetLaneSpawner()
+    {
+        AttackerSpawner[] spawners = FindObjectsOfType<AttackerSpawner>();
+
+        foreach(AttackerSpawner spawner in spawners)
+        {
+            //is defender on the same y coordinates
+            bool isCloseEnough = (Mathf.Abs(spawner.transform.position.y - transform.position.y) <= Mathf.Epsilon);
+
+            if(isCloseEnough)
+            {
+                myLaneSpawner = spawner;
+            }
+        }
+    }
+
+    private bool IsAttackerInLane()
+    {
+        return myLaneSpawner.transform.childCount > 0;
+
+        /*
+         * 
+        if (myLaneSpawner.transform.childCount <= 0)
+        {
+            return false;
+        }
+        else
+        {
+
+        }
+
+        return false;
+        */
     }
 
     public void Fire()
